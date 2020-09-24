7246838642694d6cadbda30ad1f62e26b8829c41
Yumin Chu <s2019w20@e-mirim.hs.kr>
2020-09-24 20:54:23 +0900

    p104 버튼 실행 예제

--- app/src/main/java/kr/hs/emirim/s2019w20/helloandroid/MainActivity.kt
+++ app/src/main/java/kr/hs/emirim/s2019w20/helloandroid/MainActivity.kt
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {//가장 처음 실행돔
+    override fun onCreate(savedInstanceState: Bundle?) {//가장 처음 실행됨
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)//R = resource activity_main의 경로명
     }//activity명은 숫자, 소문자, _로만 적어줄 것
